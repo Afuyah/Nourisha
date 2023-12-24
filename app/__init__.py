c0d71139df45f4853abd2a7e3e004ed01c2248ee
Afuyah <90299456+Afuyah@users.noreply.github.com>
2023-12-24 10:48:55 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,8 @@ login_manager = LoginManager()
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
-    
+    app.config['SECRET_KEY'] = 'your_secret_key'
+  
     db.init_app(app)
     login_manager.init_app(app)
     
