f84d5d81dd172ce283af62c43024e81f29dabf9f
Afuyah <90299456+Afuyah@users.noreply.github.com>
2023-12-24 09:04:01 +0000

    Application

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_wtf.csrf import CSRFProtect
-from app import create_app, db
+from app import create_app,db 
 
 # Create the Flask app within the application context
-app = create_app()
-app.config['STATIC_URL_PATH'] = '/static'
+application = create_app()
+application.config['STATIC_URL_PATH'] = '/static'
 # Initialize the database
-with app.app_context():
+with application.app_context():
     db.create_all()
+
+if __name__ == '__main__':
+    application.run(host='0.0.0.0', port=81, 
+            debug=False)
