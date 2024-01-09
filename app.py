0e7287cb76390d2eef8a3300ab6d4f2dd1c78a4f
Afuyah <90299456+Afuyah@users.noreply.github.com>
2024-01-09 19:05:05 +0000

    added wsgi.py

--- app.py
+++ app.py
@@ -1,14 +1,5 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-from flask_wtf.csrf import CSRFProtect
-from flask_mail import Mail
-from flask_migrate import Migrate
-from config import Config
-from datetime import timedelta
-from flask_cors import CORS
 from app import create_app, db
-
+from flask_migrate import Migrate
 # Import additional models and blueprints if needed
 
 app = create_app()
