db027504e72135a0e64dd9edbb8f3a56f3b8163e
Afuyah <90299456+Afuyah@users.noreply.github.com>
2024-04-12 05:59:53 +0000

    minor corrections

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ migrate = Migrate()
 
 class Config: 
     SECRET_KEY = secrets.token_hex(16)
-    SQLALCHEMY_DATABASE_URI = 'postgres://naurisha_gstores_user:5HNvexMQXLtCTWDeP1C2fssc0wFjxLVA@dpg-cocc7pol5elc739qbqsg-a.oregon-postgres.render.com/naurisha_gstores'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://naurisha_gstores_user:5HNvexMQXLtCTWDeP1C2fssc0wFjxLVA@dpg-cocc7pol5elc739qbqsg-a.oregon-postgres.render.com/naurisha_gstores'
   
 #'sqlite:///naurish_a_g_stores.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
